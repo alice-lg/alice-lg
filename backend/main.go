c6c88d621acc9f6697298bdea5a1e7cfc44f53cc
Matthias Hannig <matthias@hannig.cc>
2017-05-16 15:22:40 +0200

    added static file handling

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,32 @@
 package main
 
-import ()
+import (
+	"github.com/GeertJohan/go.rice"
+
+	"log"
+	"net/http"
+)
 
 func main() {
 	printBanner()
+
+	// Load configuration
+
+	// Serve static assets
+	assets := rice.MustFindBox("../client/build")
+	assetsHandler := http.StripPrefix(
+		"/static/",
+		http.FileServer(assets.HTTPBox()))
+
+	index, err := assets.String("index.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println(index)
+
+	http.Handle("/static/", assetsHandler)
+
+	// Start http server
+	http.ListenAndServe(":7340", nil)
 }
