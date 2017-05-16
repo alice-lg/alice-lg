f5f4e272a825b7e925a74f5846c01abcfdd94fef
Matthias Hannig <matthias@hannig.cc>
2017-05-16 13:58:25 +0200

    added api layout

---
+++ backend/api.go
@@ -0,0 +1,18 @@
+package main
+
+// Alice LG Rest API
+//
+// The API provides endpoints for getting
+// information from the routeservers / alice datasources.
+//
+// Endpoints:
+//
+//   Config
+//     Show         /api/config
+//
+//   Routeservers
+//     List         /api/routeservers
+//     Status       /api/routeservers/:id/status
+//     Neighbours   /api/routeservers/:id/neighbours
+//     Routes       /api/routeservers/:id/neighbours/:neighbour_id/routes
+//
