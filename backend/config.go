2a6cc841dec98b553ceb9b482b19441bb7d59c52
Matthias Hannig <matthias@hannig.cc>
2017-05-17 17:29:07 +0200

    parse sections and create mapping

--- backend/config.go
+++ backend/config.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 
 	"github.com/ecix/alice-lg/backend/sources/birdwatcher"
@@ -17,6 +18,27 @@ type ServerConfig struct {
 	Listen string `ini:"listen_http"`
 }
 
+type RejectionsConfig struct {
+	Asn      int `ini:"asn"`
+	RejectId int `ini:"reject_id"`
+
+	Reasons map[int]string
+}
+
+type NoexportsConfig struct {
+	Asn      int `ini:"asn"`
+	RejectId int `ini:"reject_id"`
+
+	Reasons map[int]string
+}
+
+type UiConfig struct {
+	RoutesColumns map[string]string
+
+	RoutesRejections RejectionsConfig
+	RoutesNoexports  NoexportsConfig
+}
+
 type SourceConfig struct {
 	Name string
 	Type int
@@ -27,6 +49,7 @@ type SourceConfig struct {
 
 type Config struct {
 	Server  ServerConfig
+	Ui      UiConfig
 	Sources []SourceConfig
 }
 
@@ -54,6 +77,101 @@ func getBackendType(section *ini.Section) int {
 	}
 
 	return SOURCE_UNKNOWN
+}
+
+// Get UI config: Routes Columns
+// The columns displayed in the frontend
+func getRoutesColumns(config *ini.File) (map[string]string, error) {
+	columns := make(map[string]string)
+	section := config.Section("routes_columns")
+	keys := section.Keys()
+
+	for _, key := range keys {
+		columns[key.Name()] = section.Key(key.Name()).MustString("")
+	}
+
+	return columns, nil
+}
+
+// Get UI config: Get rejections
+func getRoutesRejections(config *ini.File) (RejectionsConfig, error) {
+	reasons := make(map[int]string)
+	baseConfig := config.Section("rejection")
+	reasonsConfig := config.Section("rejection_reasons")
+
+	// Map base configuration
+	rejectionsConfig := RejectionsConfig{}
+	baseConfig.MapTo(&rejectionsConfig)
+
+	// Map reasons
+	keys := reasonsConfig.Keys()
+	for _, key := range keys {
+		id, err := strconv.Atoi(key.Name())
+		if err != nil {
+			return rejectionsConfig, err
+		}
+		reasons[id] = reasonsConfig.Key(key.Name()).MustString("")
+	}
+
+	rejectionsConfig.Reasons = reasons
+
+	return rejectionsConfig, nil
+}
+
+// Get UI config: Get no export config
+func getRoutesNoexports(config *ini.File) (NoexportsConfig, error) {
+	reasons := make(map[int]string)
+	baseConfig := config.Section("noexport")
+	reasonsConfig := config.Section("noexport_reasons")
+
+	// Map base configuration
+	noexportsConfig := NoexportsConfig{}
+	baseConfig.MapTo(&noexportsConfig)
+
+	// Map reasons for missing export
+	keys := reasonsConfig.Keys()
+	for _, key := range keys {
+		id, err := strconv.Atoi(key.Name())
+		if err != nil {
+			return noexportsConfig, err
+		}
+		reasons[id] = reasonsConfig.Key(key.Name()).MustString("")
+	}
+
+	noexportsConfig.Reasons = reasons
+
+	return noexportsConfig, nil
+}
+
+// Get the UI configuration from the config file
+func getUiConfig(config *ini.File) (UiConfig, error) {
+	uiConfig := UiConfig{}
+
+	// Get route columns
+	routesColumns, err := getRoutesColumns(config)
+	if err != nil {
+		return uiConfig, err
+	}
+
+	// Get rejections and reasons
+	rejections, err := getRoutesRejections(config)
+	if err != nil {
+		return uiConfig, err
+	}
+
+	noexports, err := getRoutesNoexports(config)
+	if err != nil {
+		return uiConfig, err
+	}
+
+	// Make config
+	uiConfig = UiConfig{
+		RoutesColumns:    routesColumns,
+		RoutesRejections: rejections,
+		RoutesNoexports:  noexports,
+	}
+
+	return uiConfig, nil
 }
 
 func getSources(config *ini.File) ([]SourceConfig, error) {
@@ -127,8 +245,15 @@ func loadConfigs(base, global, local string) (*Config, error) {
 		return nil, err
 	}
 
+	// Get UI configurations
+	ui, err := getUiConfig(parsedConfig)
+	if err != nil {
+		return nil, err
+	}
+
 	config := &Config{
 		Server:  server,
+		Ui:      ui,
 		Sources: sources,
 	}
 
