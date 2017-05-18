486adf453874531ec0e9b4a0f7cfbe0c591563fa
Matthias Hannig <matthias@hannig.cc>
2017-05-18 18:10:55 +0200

    initial birdwatcher client stub

--- backend/sources/birdwatcher/source.go
+++ backend/sources/birdwatcher/source.go
@@ -2,27 +2,35 @@ package birdwatcher
 
 import (
 	"log"
+
+	"github.com/ecix/alice-lg/backend/api"
 )
 
 type Birdwatcher struct {
-	config *Config
+	config Config
 }
 
-func NewBirdwatcher(config *Config) *Birdwatcher {
+func NewBirdwatcher(config Config) *Birdwatcher {
 	birdwatcher := &Birdwatcher{
 		config: config,
 	}
 	return birdwatcher
 }
 
-func (self *Birdwatcher) Status() {
+func (self *Birdwatcher) Status() (api.StatusResponse, error) {
 	log.Println("Implement me: Status()")
+
+	return api.StatusResponse{}, nil
 }
 
-func (self *Birdwatcher) Neighbours() {
+func (self *Birdwatcher) Neighbours() (api.NeighboursResponse, error) {
 	log.Println("Implement me: Neighbours()")
+
+	return api.NeighboursResponse{}, nil
 }
 
-func (self *Birdwatcher) Routes() {
+func (self *Birdwatcher) Routes(neighbourId string) (api.RoutesResponse, error) {
 	log.Println("Implement me: Routes()")
+
+	return api.RoutesResponse{}, nil
 }
--- backend/sources/source.go
+++ backend/sources/source.go
@@ -1,7 +1,11 @@
 package sources
 
+import (
+	"github.com/ecix/alice-lg/backend/api"
+)
+
 type Source interface {
-	Status()
-	Neighbours()
-	Routes(neighbourId int)
+	Status() (api.StatusResponse, error)
+	Neighbours() (api.NeighboursResponse, error)
+	Routes(neighbourId string) (api.RoutesResponse, error)
 }
