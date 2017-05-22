88115cb08ead477493fd868d0f77dea81c7a08dd
Matthias Hannig <matthias@hannig.cc>
2017-05-22 17:22:40 +0200

    fixed last reboot rendering

--- client/components/routeservers/status/index.jsx
+++ client/components/routeservers/status/index.jsx
@@ -19,17 +19,23 @@ class Details extends React.Component {
       return null;
     }
 
+
+    let lastReboot = rsStatus.last_reboot;
+    if (lastReboot == "0001-01-01T00:00:00Z") {
+        lastReboot = null;
+    }
+
     return (
       <div className="routeserver-status">
         <ul>
-          {rsStatus.last_reboot && 
+          {lastReboot &&
             <li><i className="fa fa-clock-o"></i>
-              Last Reboot: <b><Datetime value={rsStatus.last_reboot} /></b>
+              Last Reboot: <b><Datetime value={lastReboot} /></b>
             </li>}
-          <li><i className="fa fa-clock-o"></i> 
+          <li><i className="fa fa-clock-o"></i>
             Last Reconfig: <b><Datetime value={rsStatus.last_reconfig} /></b>
           </li>
-          <li><i className="fa fa-battery-full"></i> 
+          <li><i className="fa fa-battery-full"></i>
             <b>{rsStatus.message}</b></li>
         </ul>
       </div>
