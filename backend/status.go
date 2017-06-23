ec1a5803e67a34ee6ef48160aab6f3b67213b218
Matthias Hannig <matthias@hannig.cc>
2017-06-23 16:25:33 +0200

    added route stats status

--- backend/status.go
+++ backend/status.go
@@ -4,14 +4,21 @@ var version = "unknown"
 
 // Gather application status information
 type AppStatus struct {
-	Version string `json:"version"`
+	Version string           `json:"version"`
+	Routes  RoutesStoreStats `json:"routes"`
 }
 
 // Get application status, perform health checks
 // on backends.
 func NewAppStatus() (*AppStatus, error) {
+	routesStatus := RoutesStoreStats{}
+	if AliceRoutesStore != nil {
+		routesStatus = AliceRoutesStore.Stats()
+	}
+
 	status := &AppStatus{
 		Version: version,
+		Routes:  routesStatus,
 	}
 	return status, nil
 }
