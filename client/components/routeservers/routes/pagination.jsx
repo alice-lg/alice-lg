cbd1861f0dd21018855f9b7b86b179595e7a8f28
Matthias Hannig <matthias@hannig.cc>
2018-07-23 17:09:42 +0200

    added pagination with select

--- client/components/routeservers/routes/pagination.jsx
+++ client/components/routeservers/routes/pagination.jsx
@@ -3,16 +3,11 @@ import React from 'react'
 import {connect} from 'react-redux'
 
 import {Link} from 'react-router'
+import {push} from 'react-router-redux'
 
 
-
-const PageLink = function(props) {
+const makeLinkProps = function(props) {
   const linkPage = parseInt(props.page);
-  const label = props.label || (linkPage + 1);
-
-  if (props.disabled) {
-    return <span>{label}</span>;
-  }
 
   let pr = props.pageReceived;
   let pf = props.pageFiltered;
@@ -39,30 +34,101 @@ const PageLink = function(props) {
     search:   search,
   };
 
+  return linkTo;
+}
 
+
+const PageLink = function(props) {
+  const linkPage = parseInt(props.page);
+  const label = props.label || (linkPage + 1);
+
+  if (props.disabled) {
+    return <span>{label}</span>;
+  }
+
+  const linkTo = makeLinkProps(props);
   return (
     <Link to={linkTo}>{label}</Link>
   );
 }
 
 
+const PageSelect = (props) => {
+  const {pages, options} = props;
+
+  if (pages.length == 0) {
+    return null; // nothing to do here.
+  }
+
+  const items = pages.map((p) => (
+    <option key={p} value={p}>{p + 1}</option>
+  ));
+
+  const active = props.page >= pages[0];
+  let itemClassName = "";
+  if (active) {
+    itemClassName = "active";
+  }
+  
+  return (
+    <li className={itemClassName}>
+      <select className="pagination-select"
+              value={props.page}
+              onChange={(e) => props.onChange(e.target.value) }>
+        { props.page < pages[0] && <option value={pages[0]}>more...</option> }
+
+        {items}
+      </select>
+    </li>
+  );
+}
+
+
+
 class RoutesPaginatorView extends React.Component {
 
+  /*
+   * Create an array of page "ids" we can use to map our
+   * pagination items.
+   * Split result into items for direct link access and
+   * select for a dropdown like access.
+   */
+  makePaginationPages(numPages) {
+    const MAX_ITEMS = 12; 
+    const pages = Array.from(Array(numPages), (_, i) => i); 
+    return {
+      items:  pages.slice(0, MAX_ITEMS),
+      select: pages.slice(MAX_ITEMS)
+    }
+  }
+
+  /*
+   * Dispatch navigation event and go to page
+   */
+  navigateToPage(page) {
+    const linkProps = makeLinkProps(Object.assign({}, this.props, {
+      page: page
+    }));
+
+    this.props.dispatch(push(linkProps));
+  }
+
   render() {
+
     if (this.props.totalPages <= 1) {
       return null; // Nothing to paginate
     }
 
-
-    const pageLinks = Array.from(Array(this.props.totalPages), (_, i) => {
+    const pages = this.makePaginationPages(this.props.totalPages);
+    const pageLinks = pages.items.map((p) => {
       let className = "";
-      if (i == this.props.page) {
+      if (p == this.props.page) {
         className = "active";
       }
 
       return (
-        <li key={i} className={className}>
-          <PageLink page={i}
+        <li key={p} className={className}>
+          <PageLink page={p}
                     routing={this.props.routing}
                     anchor={this.props.anchor}
                     pageReceived={this.props.pageReceived}
@@ -71,6 +137,7 @@ class RoutesPaginatorView extends React.Component {
         </li>
       );
     });
+
 
     let prevLinkClass = "";
     if (this.props.page == 0) {
@@ -96,16 +163,21 @@ class RoutesPaginatorView extends React.Component {
                       pageNotExported={this.props.pageNotExported} />
           </li>
           {pageLinks}
-          <li className={nextLinkClass}>
-            <PageLink page={this.props.page + 1}
-                      disabled={this.props.page + 1 == this.props.totalPages}
-                      label="&raquo;"
-                      routing={this.props.routing}
-                      anchor={this.props.anchor}
-                      pageReceived={this.props.pageReceived}
-                      pageFiltered={this.props.pageFiltered}
-                      pageNotExported={this.props.pageNotExported} />
-          </li>
+          <PageSelect pages={pages.select}
+                      page={this.props.page}
+                      onChange={(page) => this.navigateToPage(page)} />
+
+          {pages.select.length == 0 &&
+            <li className={nextLinkClass}>
+              <PageLink page={this.props.page + 1}
+                        disabled={this.props.page + 1 == this.props.totalPages}
+                        label="&raquo;"
+                        routing={this.props.routing}
+                        anchor={this.props.anchor}
+                        pageReceived={this.props.pageReceived}
+                        pageFiltered={this.props.pageFiltered}
+                        pageNotExported={this.props.pageNotExported} />
+            </li>}
         </ul>
       </nav>
     );
