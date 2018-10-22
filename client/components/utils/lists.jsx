848ae64e2f4a4e0d0a55a5d62dc70db18f7c87f7
Matthias Hannig <matthias@hannig.cc>
2018-10-22 15:13:47 +0200

    added list and text helpers

---
+++ client/components/utils/lists.jsx
@@ -0,0 +1,28 @@
+
+/** 
+ * Intersect lists: [x | x <- A, x `elem` B]
+ */
+export function intersect(a, b) {
+  let res = [];
+  for (const e of a) {
+    for (const k of b) {
+      if (e==k) {
+        res.push(e);
+        break;
+      }
+    }
+  }
+  return res;
+}
+
+/**
+ * Resolve list with dict: [dict[x]||x | x <- L]
+ */
+export function resolve(dict, list) {
+  let result = [];
+  for (const e of list) {
+    result.push(dict[e]||e); 
+  }
+  return result;
+}
+
