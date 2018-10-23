73ac576b630a2ab4683070e881a375de658aa9f1
Matthias Hannig <matthias@hannig.cc>
2018-10-23 22:32:52 +0200

    merge filters unique

--- client/components/filters/state.jsx
+++ client/components/filters/state.jsx
@@ -1,4 +1,6 @@
 
+import _ from 'underscore'
+
 import {FILTER_GROUP_SOURCES,
         FILTER_GROUP_ASNS,
         FILTER_GROUP_COMMUNITIES,
@@ -62,3 +64,86 @@ export function decodeFiltersApplied(params) {
 
   return groups;
 }
+
+/*
+ * Merge filters
+ */
+export function mergeFilters(a, b) {
+  let groups = cloneFilters(initialFilterState);
+  let setCmp = [];
+  setCmp[FILTER_GROUP_SOURCES] = cmpFilterValue;
+  setCmp[FILTER_GROUP_ASNS] = cmpFilterValue;
+  setCmp[FILTER_GROUP_COMMUNITIES] = cmpFilterCommunity;
+  setCmp[FILTER_GROUP_EXT_COMMUNITIES] = cmpFilterCommunity;
+  setCmp[FILTER_GROUP_LARGE_COMMUNITIES] = cmpFilterCommunity;
+
+  for (const i in groups) {
+    groups[i].filters = mergeFilterSet(setCmp[i], a[i].filters, b[i].filters);
+  }
+
+  return groups;
+}
+
+/*
+ * Merge list of filters
+ */
+function mergeFilterSet(inSet, a, b) {
+  let result = a;
+  for (const f of b) {
+    if (inSet(result, f)) {
+      continue;
+    }
+    result.push(f);
+  }
+  return result;
+}
+
+/*
+ * Does a single group have any filters?
+ */
+export function groupHasFilters(group) {
+  return group.filters.length > 0;
+}
+
+/*
+ * Filters set compare
+ */
+function cmpFilterValue(set, filter) {
+  for (const f of set) {
+    if(f.value == filter.value) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function cmpFilterCommunity(set, filter) {
+  for (const f of set) {
+    let match = true;
+    for (const i in f.value) {
+      if (f.value[i] != filter.value[i]) {
+        match = false;
+        break;
+      }
+    }
+
+    if (match) {
+      return true;
+    }
+  }
+  return false;
+}
+
+/*
+ * Do we have filters in general?
+ */
+export function hasFilters(groups) {
+  for (const g of groups) {
+    if (groupHasFilters(g)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+
