a6f4a6d19c31e8123fdc2d7a2e09b8e025d6d50e
Matthias Hannig <matthias@hannig.cc>
2018-12-09 18:16:41 +0100

    removed old source artifact

--- backend/sources/birdwatcher/parsers.go
+++ backend/sources/birdwatcher/parsers.go
@@ -166,12 +166,11 @@ func parseNeighbours(bird ClientResponse, config Config) (api.Neighbours, error)
 			State:       mustString(protocol["state"], "unknown"),
 			Description: mustString(protocol["description"], "no description"),
 			//TODO make these changes configurable
-			RoutesReceived:     mustInt(routes["imported"], 0),
-			RoutesExported:     mustInt(routes["exported"], 0), //TODO protocol_exported?
-			RoutesFiltered:     mustInt(routes["filtered"], 0),
-			RoutesPreferred:    mustInt(routes["preferred"], 0),
-			RoutesAccepted:     mustInt(routes["pipe_imported"], mustInt(routes["imported"], 0)),
-			RoutesPipeFiltered: mustInt(routes["pipe_filtered"], mustInt(routes["filtered"], 0)),
+			RoutesReceived:  mustInt(routes["imported"], 0),
+			RoutesExported:  mustInt(routes["exported"], 0), //TODO protocol_exported?
+			RoutesFiltered:  mustInt(routes["filtered"], 0),
+			RoutesPreferred: mustInt(routes["preferred"], 0),
+			RoutesAccepted:  mustInt(routes["pipe_imported"], mustInt(routes["imported"], 0)),
 
 			Uptime:    uptime,
 			LastError: lastError,
