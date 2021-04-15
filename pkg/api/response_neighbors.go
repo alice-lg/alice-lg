fb3bc1b745af24f57b7875686c88e9f6bbfa54fc
Annika Hannig <annika@hannig.cc>
2021-04-15 17:46:52 +0200

    added debugging helper

--- pkg/api/response_neighbors.go
+++ pkg/api/response_neighbors.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"encoding/json"
 	"strings"
 	"time"
 )
@@ -27,6 +28,13 @@ type Neighbour struct {
 
 	// Original response
 	Details map[string]interface{} `json:"details"`
+}
+
+// String encodes a neighbor as json. This is
+// more readable than the golang default represenation.
+func (n *Neighbour) String() string {
+	repr, _ := json.Marshal(n)
+	return string(repr)
 }
 
 // Implement sorting interface for routes
