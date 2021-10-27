aa9292b74cdf7d3db7d1db086dc96ae9fcc2f4b7
Annika Hannig <annika@hannig.cc>
2021-10-27 14:49:58 +0000

    moved test

---
+++ pkg/decoders/prefix_test.go
@@ -0,0 +1,31 @@
+package decoders
+
+import (
+	"testing"
+)
+
+func TestMaybePrefix(t *testing.T) {
+	expected := []struct {
+		string
+		bool
+	}{
+		{"10.0.0", true},
+		{"23.42.11.42/23", true},
+		{"fa42:2342::/32", true},
+		{"200", true},
+		{"2001:", true},
+		{"A", true},
+		{"A b", false},
+		{"23 Foo", false},
+		{"Nordfoo", false},
+		{"122.beef:", true}, // sloppy
+		{"122.beef:", true}, // very
+		{"122:beef", true},  // sloppy.
+	}
+
+	for _, e := range expected {
+		if MaybePrefix(e.string) != e.bool {
+			t.Error("Expected", e.string, "to be prefix:", e.bool)
+		}
+	}
+}
--- pkg/store/utils_test.go
+++ pkg/store/utils_test.go
@@ -1,4 +1,4 @@
-package store 
+package store
 
 import (
 	"testing"
@@ -9,30 +9,3 @@ func TestContainsCi(t *testing.T) {
 		t.Error("An unexpected error occured.")
 	}
 }
-
-func TestMaybePrefix(t *testing.T) {
-	expected := []struct {
-		string
-		bool
-	}{
-		{"10.0.0", true},
-		{"23.42.11.42/23", true},
-		{"fa42:2342::/32", true},
-		{"200", true},
-		{"2001:", true},
-		{"A", true},
-		{"A b", false},
-		{"23 Foo", false},
-		{"Nordfoo", false},
-		{"122.beef:", true}, // sloppy
-		{"122.beef:", true}, // very
-		{"122:beef", true},  // sloppy.
-	}
-
-	for _, e := range expected {
-		if MaybePrefix(e.string) != e.bool {
-			t.Error("Expected", e.string, "to be prefix:", e.bool)
-		}
-	}
-}
-
