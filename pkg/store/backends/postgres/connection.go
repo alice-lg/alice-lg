3241a150aab85a99e82073824b367334937f9fc5
Annika Hannig <annika@hannig.cc>
2022-01-11 14:36:49 +0100

    initial schema and initial postgres connection

---
+++ pkg/store/backends/postgres/connection.go
@@ -0,0 +1,48 @@
+package postgres
+
+import (
+	"context"
+	"errors"
+	"os"
+	"path/filepath"
+
+	"github.com/jackc/pgx/v4/pgxpool"
+)
+
+var (
+	// ErrNotInitialized will be returned if the
+	// database pool is accessed before initializing using
+	// Connect.
+	ErrNotInitialized = errors.New("store not initialized, pool not ready")
+
+	// ErrMaxConnsUnconfigured will be returned, if the
+	// the maximum connections are zero.
+	ErrMaxConnsUnconfigured = errors.New("MaxConns not configured")
+)
+
+// ConnectOpts database connection options
+type ConnectOpts struct {
+	URL      string
+	MaxConns int32
+	MinConns int32
+}
+
+// Connect creates and configures a pgx pool
+func Connect(ctx context.Context, opts *ConnectOpts) (*pgxpool.Pool, error) {
+	// Initialize postgres connection
+	cfg, err := pgxpool.ParseConfig(opts.URL)
+	if err != nil {
+		return nil, err
+	}
+
+	cfg.ConnConfig.RuntimeParams["application_name"] = filepath.Base(os.Args[0])
+	if opts.MaxConns == 0 {
+		return nil, ErrMaxConnsUnconfigured
+	}
+
+	// We need some more connections
+	cfg.MaxConns = opts.MaxConns
+	cfg.MinConns = opts.MinConns
+
+	return pgxpool.ConnectConfig(ctx, cfg)
+}
---
+++ pkg/store/backends/postgres/schema.sql
@@ -0,0 +1,58 @@
+
+--
+-- ----------------------
+-- AliceLG schema v.1.0.0
+-- ----------------------
+--
+-- %% Author:      annika
+-- %% Description: Create alice-lg db schema.
+--
+
+-- Neighbors
+CREATE TABLE neighbors (
+    id    VARCHAR(255) NOT NULL PRIMARY KEY,
+
+    -- Indexed attributes
+    rs_id VARCHAR(255) NOT NULL,
+
+    -- JSON serialized neighbor
+    neighbor     jsonb NOT NULL,
+
+    -- Timestamps
+    updated_at  TIMESTAMP  NOT NULL DEFAULT CURRENT_TIMESTAMP,
+);
+
+CREATE INDEX idx_neighbors_rs_id 
+          ON neighbors            USING HASH (rs_id);
+
+-- Routes
+CREATE TABLE routes (
+    id            VARCHAR(255) NOT NULL PRIMARY KEY,
+    neighbor_id   VARCHAR(255) NOT NULL
+                  REFERENCES   neighbors(id)
+                  ON DELETE    CASCADE,
+
+    -- Indexed attributes 
+    network       cidr         NOT NULL,
+   
+    -- JSON serialized route
+    route         jsonb        NOT NULL,
+
+    -- Timestamps
+    updated_at  TIMESTAMP  NOT NULL DEFAULT CURRENT_TIMESTAMP,
+);
+
+CREATE INDEX idx_routes_network ON routes ( network );
+
+
+-- The meta table stores information about the schema
+-- like when it was migrated and the current revision.
+CREATE TABLE __meta__ (
+    version     INTEGER   NOT NULL  UNIQUE,
+    description TEXT      NOT NULL,
+    applied_at  TIMESTAMP NOT NULL  DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO __meta__ (version, description)
+     VALUES (1, 'initial schema');
+
