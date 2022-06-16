d2c20c74d5ba9d1db3844fe2b5524342d2023fbb
Annika Hannig <annika@hannig.cc>
2022-06-16 12:09:53 +0000

    initial base app

---
+++ pkg/ui/src/app/App.js
@@ -0,0 +1,18 @@
+
+
+/**
+ * Alice (formerly known as Birdseye) UI
+ * -------------------------------------
+ *
+ * @author Annika Hannig <annika@hannig.cc>
+ */
+
+
+const App = () => {
+  return (
+    <p>hej there</p>
+  );
+}
+
+export default App;
+
--- pkg/ui/src/index.js
+++ pkg/ui/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import './scss/main.scss';
+
+import App from './app/App';
 
 ReactDOM.render(
   <React.StrictMode>
