61962f2eefa8197a1beaf398ddac05785f13ea6b
Annika Hannig <annika@hannig.cc>
2022-06-16 12:14:51 +0000

    added bootstrap v3

--- pkg/ui/src/index.js
+++ pkg/ui/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 
+import 'bootstrap/dist/css/bootstrap.css';
 import './scss/main.scss';
 
 import App from './app/App';
