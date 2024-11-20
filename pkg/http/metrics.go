bc395d021d25c76e5cc385e5c79625139a3b9624
Annika Hannig <annika@hannig.cc>
2024-11-20 16:10:56 +0100

    some refactoring

--- pkg/http/metrics.go
+++ pkg/http/metrics.go
@@ -13,14 +13,21 @@ import (
 	"github.com/alice-lg/alice-lg/pkg/api"
 )
 
-func (s *Server) registerMetrics(
-	ctx context.Context,
-	router *httprouter.Router,
-) error {
-	if s.cfg.Server.EnablePrometheus == false {
-		return nil
-	}
+// All metrics are grouped here.
+type metrics struct {
+	server *Server
 
+	neighborInfo   *prometheus.GaugeVec
+	neighborUptime *prometheus.GaugeVec
+
+	routesReceived  *prometheus.GaugeVec
+	routesFiltered  *prometheus.GaugeVec
+	routesPreferred *prometheus.GaugeVec
+	routesAccepted  *prometheus.GaugeVec
+}
+
+// Create and initialize metrics
+func initMetrics(server *Server) *metrics {
 	labels := []string{
 		// The route server ID
 		"route_server_id",
@@ -32,19 +39,17 @@ func (s *Server) registerMetrics(
 		"neighbor_address",
 	}
 
-	s.routesStore.Stats(ctx)
-
-	neighborState := prometheus.NewGaugeVec(
+	neighborInfo := prometheus.NewGaugeVec(
 		prometheus.GaugeOpts{
-			Name: "neighbor_state",
-			Help: "The state of a neighbor in a route server (0 = down, 1 = up)",
+			Name: "neighbor_info",
+			Help: "Information about the neighbor including the state",
 		},
-		labels,
+		append(labels, "neighbor_state"),
 	)
 
 	neighborUptime := prometheus.NewGaugeVec(
 		prometheus.GaugeOpts{
-			Name: "neighbor_uptime",
+			Name: "neighbor_uptime_seconds_total",
 			Help: "The uptime of a neighbor on a route server in seconds",
 		},
 		labels,
@@ -82,64 +87,117 @@ func (s *Server) registerMetrics(
 		labels,
 	)
 
-	prometheus.MustRegister(neighborState)
+	prometheus.MustRegister(neighborInfo)
 	prometheus.MustRegister(neighborUptime)
 	prometheus.MustRegister(routesReceived)
 	prometheus.MustRegister(routesFiltered)
 	prometheus.MustRegister(routesPreferred)
 	prometheus.MustRegister(routesAccepted)
 
-	log.Println("Prometheus metrics enabled, listening on /metrics")
+	return &metrics{
+		server: server,
+
+		neighborInfo:   neighborInfo,
+		neighborUptime: neighborUptime,
+
+		routesReceived:  routesReceived,
+		routesFiltered:  routesFiltered,
+		routesPreferred: routesPreferred,
+	}
+}
+
+// Update metrics
+func (m *metrics) update(ctx context.Context) error {
+	res, err := m.server.apiRouteServersList(ctx, nil, nil)
+	if err != nil {
+		return err
+	}
+	routeServers := res.(api.RouteServersResponse).RouteServers
+
+	// For all route servers, fetch neighbors list and
+	// update statistics.
+	for _, rs := range routeServers {
+		res, err = m.server.apiNeighborsList(
+			ctx, nil, httprouter.Params{
+				{Key: "id", Value: rs.ID},
+			})
+		if err != nil {
+			return err
+		}
+		neighbors := res.(*api.NeighborsResponse).Neighbors
+
+		// Get neighbors
+		for _, neighbor := range neighbors {
+
+			m.neighborInfo.With(prometheus.Labels{
+				"route_server_id":      rs.ID,
+				"route_server_name":    rs.Name,
+				"route_server_group":   rs.Group,
+				"neighbor_id":          neighbor.ID,
+				"neighbor_description": neighbor.Description,
+				"neighbor_asn":         strconv.Itoa(neighbor.ASN),
+				"neighbor_address":     neighbor.Address,
+				"neighbor_state":       neighbor.State,
+			}).Set(1.0)
+
+			labels := prometheus.Labels{
+				"route_server_id":      rs.ID,
+				"route_server_name":    rs.Name,
+				"route_server_group":   rs.Group,
+				"neighbor_id":          neighbor.ID,
+				"neighbor_description": neighbor.Description,
+				"neighbor_asn":         strconv.Itoa(neighbor.ASN),
+				"neighbor_address":     neighbor.Address,
+			}
+			m.neighborUptime.
+				With(labels).
+				Set(neighbor.Uptime.Seconds())
+			m.routesReceived.
+				With(labels).
+				Set(float64(neighbor.RoutesReceived))
+			m.routesFiltered.
+				With(labels).
+				Set(float64(neighbor.RoutesFiltered))
+			m.routesPreferred.
+				With(labels).
+				Set(float64(neighbor.RoutesPreferred))
+			m.routesAccepted.
+				With(labels).
+				Set(float64(neighbor.RoutesAccepted))
+		}
+	}
+
+	return nil
+}
+
+func (s *Server) registerMetrics(
+	ctx context.Context,
+	router *httprouter.Router,
+) error {
+	if s.cfg.Server.EnableMetrics == false {
+		return nil
+	}
+
+	m := initMetrics(s)
+
+	log.Println("Metrics enabled and available on: /metrics")
 	router.Handler("GET", "/metrics", promhttp.Handler())
 
+	// Every 5 second, update the metrics
 	go func() {
-		// Every 5 second, update the metrics with the latest data from the cache
 		for {
 			select {
 			case <-ctx.Done():
 				return
 			case <-time.After(time.Second * 5):
-				routeServers, err := s.apiRouteServersList(ctx, nil, nil)
+				err := m.update(ctx)
 				if err != nil {
-					log.Println("[metrics] error getting route servers: ", err)
-					continue
-				}
-
-				for _, routeServer := range routeServers.(api.RouteServersResponse).RouteServers {
-					neighbors, err := s.apiNeighborsList(ctx, nil, httprouter.Params{{Key: "id", Value: routeServer.ID}})
-					if err != nil {
-						log.Println("[metrics] error getting neighbors:", err)
-						continue
-					}
-
-					for _, neighbor := range neighbors.(*api.NeighborsResponse).Neighbors {
-						labels := prometheus.Labels{
-							"route_server_id":      routeServer.ID,
-							"route_server_name":    routeServer.Name,
-							"route_server_group":   routeServer.Group,
-							"neighbor_id":          neighbor.ID,
-							"neighbor_description": neighbor.Description,
-							"neighbor_asn":         strconv.Itoa(neighbor.ASN),
-							"neighbor_address":     neighbor.Address,
-						}
-
-						state := float64(0)
-						if neighbor.State == "up" {
-							state = 1
-						}
-
-						neighborState.With(labels).Set(state)
-						neighborUptime.With(labels).Set(neighbor.Uptime.Seconds())
-						routesReceived.With(labels).Set(float64(neighbor.RoutesReceived))
-						routesFiltered.With(labels).Set(float64(neighbor.RoutesFiltered))
-						routesPreferred.With(labels).Set(float64(neighbor.RoutesPreferred))
-						routesAccepted.With(labels).Set(float64(neighbor.RoutesAccepted))
-					}
+					log.Println(
+						"[metrics] Error while updating:", err)
 				}
 			}
-
 		}
-
 	}()
+
 	return nil
 }
