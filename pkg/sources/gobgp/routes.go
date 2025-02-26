fb90181192d1fd1d7bdfe011dd1d9baed8f671bb
Alexey Mekhanoshin <367257+Ales999@users.noreply.github.com>
2025-02-26 23:44:14 +0300

    Clean old code

--- pkg/sources/gobgp/routes.go
+++ pkg/sources/gobgp/routes.go
@@ -15,17 +15,6 @@ import (
 	"github.com/alice-lg/alice-lg/pkg/sources/gobgp/apiutil"
 )
 
-/*
-var families []gobgpapi.Family = []gobgpapi.Family{{
-	Afi:  gobgpapi.Family_AFI_IP,
-	Safi: gobgpapi.Family_SAFI_UNICAST,
-}, {
-	Afi:  gobgpapi.Family_AFI_IP6,
-	Safi: gobgpapi.Family_SAFI_UNICAST,
-},
-}
-*/
-
 // NewRoutesResponse creates a new routes response
 func NewRoutesResponse() api.RoutesResponse {
 	routes := api.RoutesResponse{}
